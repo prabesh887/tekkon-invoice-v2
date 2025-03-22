a2e42649e8954b82372979970099a830ad01cd95
prabesh887 <prabesh@tekkon.com.np>
2025-03-22 16:36:17 +0545

    fix: update style css for tekkon theme color

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="flex p-5 justify-start bg-primary text-white sticky border-b-2 border-b-white uppercase tracking-[10px]">
+      <a className="font-semibold text-white cursor-pointer" href="#">
+        Payslip
+      </a>
+    </div>
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button"
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <Button>Button</Button>
-    </div>
+    <>
+      <Header />
+      <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20"></div>
+    </>
   )
 }
